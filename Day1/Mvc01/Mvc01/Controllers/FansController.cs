2566eea33915a2399fd79fc4b384b8d059bce84a
sumet.funan@outlook.com <sumet.funan@outlook.com>
2019-05-09 00:46:30 +0700

    [Fan] Add fan

---
+++ Day1/Mvc01/Mvc01/Controllers/FansController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mvc01.Models;
+
+namespace Mvc01.Controllers
+{
+    public class FansController : Controller
+    {
+        private static Fan fan = new Fan();
+        public IActionResult Index()
+        {
+            return View(fan);
+        }
+
+        public IActionResult PushNumber(int number)
+        {
+            fan.AcceptNumber(number);
+            if (fan.Number > 0)
+            {
+                if (fan.IsPlugin)
+                {
+                    fan.PowerOn();
+                }
+            }
+            else
+            {
+                fan.PowerOff();
+            }
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult ControlShook(bool isShook)
+        {
+            if (isShook)
+            {
+                fan.AcceptPush();
+            }
+            else
+            {
+                fan.AcceptPull();
+            }
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult ControlPlug(bool isPlugin)
+        {
+            if (isPlugin)
+            {
+                fan.Plugin();
+                if (fan.Number > 0)
+                {
+                    fan.PowerOn();
+                }
+            }
+            else
+            {
+                fan.UnPlug();
+                fan.PowerOff();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
--- Day1/Mvc01/Mvc01/Models/Machine.cs
+++ Day1/Mvc01/Mvc01/Models/Machine.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Mvc01.Models
+﻿namespace Mvc01.Models
 {
     public class Machine
     {
