ff9272e97d9c4bf087e7b6403907621370845096
sumet.funan@outlook.com <sumet.funan@outlook.com>
2019-05-11 14:08:58 +0700

    Add areas and nav-var dark mode.

---
+++ Day1/Mvc01/Mvc01/Areas/Admin/Controllers/SupportsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mvc01.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SupportsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Day1/Mvc01/Mvc01/Startup.cs
+++ Day1/Mvc01/Mvc01/Startup.cs
@@ -56,6 +56,11 @@ namespace Mvc01
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
+                    name: "areas",
+                    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                  );
+
+                routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
