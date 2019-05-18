8a6ea288c821f9a42a3597b26c34834ceda51ac4
sumet.funan@outlook.com <sumet.funan@outlook.com>
2019-05-18 11:58:53 +0700

    Add other project

---
+++ Day1/mvc16/Hello/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "World"; 
+            if (args.Length > 0)
+            {
+                name = args[0];
+            }
+            
+            Console.WriteLine($"Hello {name}!");
+        }
+    }
+}
