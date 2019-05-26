ebfc83888ba1a72eebbeb49e20f81b7f187324bb
sumet.funan <sumet.funan@outlook.com>
2019-05-26 12:44:37 +0700

    Add entity ProductPicture

---
+++ Day1/Mvc01/Mvc01.Models/ProductPicture.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Mvc01.Models
+{
+    //[Table("ProductPictures")]
+    public class ProductPicture
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        [Required]
+        [StringLength(512)]
+        public string Url { get; set; }
+    }
+}
--- Day1/Mvc01/Mvc01/Migrations/20190526044233_update08.Designer.cs
+++ Day1/Mvc01/Mvc01/Migrations/20190526044233_update08.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Mvc01.Data;
 
 namespace Mvc01.Migrations
 {
     [DbContext(typeof(AppDb))]
-    partial class AppDbModelSnapshot : ModelSnapshot
+    [Migration("20190526044233_update08")]
+    partial class update08
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
