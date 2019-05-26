f100d43ca9b91c25b9b157f82a6aec2bc8331532
sumet.funan <sumet.funan@outlook.com>
2019-05-26 16:26:35 +0700

    Add entity slot and modify machine

---
+++ Day1/Mvc01/Mvc01.Models/Slot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Mvc01.Models
+{
+    public class Slot
+    {
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+        [Required]
+        public Machine Machine { get; set; }
+        public Product Product { get; set; }
+    }
+}
