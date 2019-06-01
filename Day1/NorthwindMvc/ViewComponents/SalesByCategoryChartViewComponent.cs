1d99b3dc2813d3a458a95e8ff1181d1f939f1204
sumet.funan <sumet.funan@outlook.com>
2019-06-01 16:59:33 +0700

    Add Chart

---
+++ Day1/NorthwindMvc/ViewComponents/SalesByCategoryChartViewComponent.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NorthwindMvc.Northwind.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NorthwindMvc.ViewComponents
+{
+    public class SalesByCategoryChartViewComponent : ViewComponent
+    {
+        private readonly AppDb db;
+
+        public SalesByCategoryChartViewComponent(AppDb db)
+        {
+            this.db = db;
+        }
+
+        public IViewComponentResult Invoke(string category, int year)
+        {
+            var items = db.sp_SalesByCategories(category, year);
+
+            var names = string.Join(",",
+                        items.Select(x => "'" + x.ProductName.Replace("'", "") + "'")
+                        .ToArray());
+            ViewBag.ProductNameList = $"[{names}]";
+
+            var totals = string.Join(",",
+                        items.Select(x => x.Total).ToArray());
+            ViewBag.TotalList = $"[{totals}]";
+
+            return View(items);
+        }
+    }
+}
